78cf873a5d74ebde73bf988bec15212d94a3db1c
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2021-05-04 16:39:55 +0900

    [update]スタイルを当てた

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,150 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled, { createGlobalStyle } from 'styled-components'
+import * as color from './color'
 
 function App() {
   return (
-    <div>
-      <header>
-        <div>KanbanBord</div>
-        <input placeholder="Filter cards" />
-      </header>
+       <Container>
+        <Header>
+          <Logo>KanbanBord</Logo>
+          <CardFilter placeholder="Filter cards" />
+        </Header>
 
-      <div>
-        <section>
-          <h3>TODO</h3>
-          <article>朝食をとる🍞</article>
-          <article>SNSをチェックする🐦</article>
-          <article>布団に入る (:3[___]</article>
-        </section>
+        <MainArea>
+          <HorizontalScroll>
+            <Column>
+              <ColumnHeader>TODO</ColumnHeader>
+              <Card>朝食をとる🍞</Card>
+              <Card>SNSをチェックする🐦</Card>
+              <Card>布団に入る (:3[___]</Card>
+            </Column>
 
-        <section>
-          <h3>Doing</h3>
-          <article>顔を洗う👐</article>
-          <article>歯を磨く🦷</article>
-        </section>
+            <Column>
+              <ColumnHeader>Doing</ColumnHeader>
+              <Card>顔を洗う👐</Card>
+              <Card>歯を磨く🦷</Card>
+            </Column>
 
-        <section>
-          <h3>Waiting</h3>
-        </section>
+            <Column>
+              <ColumnHeader>Waiting</ColumnHeader>
+            </Column>
 
-        <section>
-          <h3>Done</h3>
+            <Column>
+              <ColumnHeader>Done</ColumnHeader>
 
-          <article>布団から出る (:3っ)っ -=三[＿＿]</article>
-        </section>
-      </div>
-    </div>
+              <Card>布団から出る (:3っ)っ -=三[＿＿]</Card>
+            </Column>
+          </HorizontalScroll>
+        </MainArea>
+      </Container>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const Container = styled.div`
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+`
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 16px;
+  background-color: ${color.Navy};
+  flex-shrink: 0;
+  color: ${color.Silver};
+  font-size: 16px;
+  font-weight: bold;
+`
+
+const Logo = styled.div`
+  height: 100%;
+  padding: 16px 0;
+  overflow-y: auto;
+`
+
+const CardFilter = styled.input`
+  display: flex;
+  align-items: center;
+  min-width: 300px;
+  border: solid 1px ${color.Silver};
+  border-radius: 3px;
+  color: ${color.White};
+  padding: 5px;
+`
+
+const MainArea = styled.div`
+  height: 100%;
+  padding: 16px 0;
+  overflow-y: auto;
+`
+
+const HorizontalScroll = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  overflow-x: auto;
+
+  > * {
+    margin-left: 16px;
+    flex-shrink: 0;
+  }
+
+  ::after {
+    display: block;
+    flex: 0 0 16px;
+    content: '';
+  }
+ `
+
+const Column = styled.div`
+  display: flex;
+  flex-flow: column;
+  width: 355px;
+  height: 100%;
+  border: solid 1px ${color.Silver};
+  border-radius: 6px;
+  background-color: ${color.LightSilver};
+
+  > :not(:last-child) {
+    flex-shrink: 0;
+  }
+`
+const ColumnHeader = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 8px;
+`
+
+const Card = styled.div`
+  position: relative;
+  border: solid 1px ${color.Silver};
+  border-radius: 6px;
+  box-shadow: 0 1px 3px hsla(0, 0%, 7%, 0.1);
+  padding: 8px 32px;
+  background-color: ${color.White};
+  cursor: move
+`
+
+const GlobalStyle = createGlobalStyle`
+html, body, #app {
+  height: 100%;
+}
+body {
+  /* https://css-tricks.com/snippets/css/system-font-stack/ */
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+  Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+
+  overflow-wrap: break-word;
+}
+`
+
+ReactDOM.render(
+  <>
+    <GlobalStyle/>
+    <App />
+  </>,
+document.getElementById('app'))
