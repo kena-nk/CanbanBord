624ab74cbcd36d930997c38f94ee6a6d505a6963
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2021-05-04 14:01:55 +0900

    [add]prettier導入してフォーマットした

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 function App() {
-    return <h1>Hello React!</h1>;
+  return <h1>Hello React!</h1>
 }
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.render(<App />, document.getElementById('app'))
